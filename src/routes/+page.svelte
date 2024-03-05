822ade0a0b56cdcba8b2cffc726e8de19a8efb4a
Martin Mayrhofer <Mayrhofer.Martin15@gmail.com>
2024-03-05 13:14:00 +0100

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,27 @@
 <script>
-    import {supabase} from "$lib/supabase";
-    
-    let promise = supabase.from("countries").select();
+	import { supabase } from '$lib/supabase';
+
+	let promise = supabase.from('countries').select();
 </script>
+
 <div class="prose">
-<h1>Aufgaben:</h1>
+	<h1>Aufgaben:</h1>
 
-<ul>
-
-    <li>anstatt eines JS-Objekts das auf der Seite dargestellt wird, möchte ich ein Liste aller Länder</li>
-    <li>
-        integriere einen loading Spinner von DaisyUI in dein Projekt
-    </li>
-    <li> gibt die Vercel URL zu deinem Projekt via Aufgab in Teams ab.</li>
-
-</ul>
+	<ul>
+		<li>
+			anstatt eines JS-Objekts das auf der Seite dargestellt wird, möchte ich ein Liste aller Länder
+		</li>
+		<li>integriere einen loading Spinner von DaisyUI in dein Projekt</li>
+		<li>gibt die Vercel URL zu deinem Projekt via Aufgab in Teams ab.</li>
+	</ul>
 </div>
 
 {#await promise}
-    <div><span class="loading loading-dots loading-lg" /></div>
+	<div><span class="loading loading-dots loading-lg" /></div>
 {:then result}
-    <ul>
-        {#each result.data as country}
-            <li>{country.name}</li>
-        {/each}
-    </ul>
+	<ul>
+		{#each result.data as country}
+			<li>{country.name}</li>
+		{/each}
+	</ul>
 {/await}
