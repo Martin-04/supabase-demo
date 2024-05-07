2aa8f1a781ed35315104972516e1a271a5d3a7ef
Martin Mayrhofer <Mayrhofer.Martin15@gmail.com>
2024-05-07 13:19:55 +0200

    ok

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,25 @@
 <script>
 	import { onMount } from 'svelte';
-	import {supabase} from '$lib/supabase';
+	import { supabase } from '$lib/supabase';
 	import '../app.postcss';
-	import {session, theme} from '$lib/store';
+	import { session, theme } from '$lib/store';
 	onMount(() => {
-    supabase.auth.getSession().then(({ data }) => {
-      $session = data.session
-    })
+		supabase.auth.getSession().then(({ data }) => {
+			$session = data.session;
+		});
 
-    supabase.auth.onAuthStateChange((_event, _session) => {
-      $session = _session
-    })
-  })
+		supabase.auth.onAuthStateChange((_event, _session) => {
+			$session = _session;
+		});
+	});
 </script>
 
 <div data-theme={theme} class="min-h-screen p-3">
 	<nav class="flex items-center gap-8 m-4">
 		<a href="/" class="text-center">Home</a>
-			<a href="/profile" class="text-center">Profile</a>
-			<a href="/login" class="text-center">Login</a>
-		
+		<a href="/profile" class="text-center">Profile</a>
+		<a href="/login" class="text-center">Login</a>
+		<a href="/accounts" class="text-center">Account</a>
 	</nav>
 
 	<select bind:value={$theme} class="fixed top-0 right-0">
